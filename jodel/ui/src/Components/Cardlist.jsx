411a0b414be56c917ae1084041e5ff11e3e56f01
Evan <2248392717@qq.com>
2022-12-09 19:00:53 +0200

    add scroll down function

--- jodel/ui/src/Components/Cardlist.jsx
+++ jodel/ui/src/Components/Cardlist.jsx
@@ -1,26 +1,30 @@
 import {useState, useEffect,useMemo} from 'react';
 import Card from './Card.jsx';
+import List from 'rc-virtual-list';
 import { Link,useNavigate } from "react-router-dom";
+
+
+
 export default function CardList() {
 
-    // [messageId,content,vote,user_id,time])
     const [textValue,setTextValue]=useState("");
     const [messages,setMessages]=useState([]);
-    
-    useEffect(()=>{
-        const fetchData = async () => {
-            let token=localStorage.getItem("token");
-            const resp=await fetch(`http://localhost:7800/api/`,{
-            method:"GET",
-            headers:{
-                "Authorization":localStorage.getItem("token")
-            }})
-            const data=await resp.json()
-            setMessages(data);
-            if(!token){
-                localStorage.setItem("token",await resp.headers.get("Authorization"));
-            }
+
+    const fetchData = async () => {
+        let token=localStorage.getItem("token");
+        const resp=await fetch(`http://localhost:7800/api?offset=${messages.length}`,{
+        method:"GET",
+        headers:{
+            "Authorization":localStorage.getItem("token")
+        }})
+        const data=await resp.json()
+        setMessages(message=>message.concat(data));
+        if(!token){
+            localStorage.setItem("token",await resp.headers.get("Authorization"));
         }
+    }
+
+    useEffect(()=>{
         fetchData();
     },[]);
 
@@ -44,16 +48,31 @@ export default function CardList() {
         window.location.reload();
     }
 
+    const onScroll = (e) => {
+        if (e.currentTarget.scrollHeight - e.currentTarget.scrollTop === ContainerHeight) {
+            fetchData();
+        }
+    };
+
+    const ContainerHeight=500;
     return (
-        <div className="cardlist">
-            {messages.map((message)=>{
-                return <Card message={message} key={message[0]}/>
-            })}
-                <textarea id="message" rows="4" value={textValue} onChange={handleTextChange} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500
+        <div className="cardlist p-4">
+            <textarea id="message" rows="4" value={textValue} onChange={handleTextChange} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500
                 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" ></textarea>
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded" onClick={handleClickButton}>
                 new message
-                </button>
+            </button>
+                <List
+                data={messages}
+                height={ContainerHeight}
+                itemHeight={50}
+                onScroll={onScroll}
+                itemKey={(index, data) => index}
+                >
+                    {(message)=>{
+                        return <Card message={message} key={message[0]}/>
+                    }}
+                </List>
         </div>
     )
 }
